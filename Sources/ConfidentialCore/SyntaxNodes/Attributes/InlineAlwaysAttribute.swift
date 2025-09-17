c5933c29eefc0850c5957d9dbd5920eda6047480
Vitalii Budnik <nekrich@users.noreply.github.com>
2025-09-17 13:34:25 +0300

    chore: bump swift-syntax version

--- Sources/ConfidentialCore/SyntaxNodes/Attributes/InlineAlwaysAttribute.swift
+++ Sources/ConfidentialCore/SyntaxNodes/Attributes/InlineAlwaysAttribute.swift
@@ -3,11 +3,6 @@ import SwiftSyntax
 extension AttributeSyntax {
 
     static var inlineAlways: Self {
-        .init(
-            attributeName: IdentifierTypeSyntax(name: .identifier("inline")),
-            leftParen: .leftParenToken(),
-            arguments: .token(.identifier("__always")),
-            rightParen: .rightParenToken()
-        )
+        .init("inline(__always)")
     }
 }
